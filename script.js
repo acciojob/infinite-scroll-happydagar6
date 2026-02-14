3fcb5ba4186397134a485cce4b293cd9ec5b923b
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-14 11:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here!
+const list = document.getElementById("infi-list");
+let count = 1;
 
+function addItems(num) {
+	for(let i = 0; i < num; i++) {
+		const li = document.createElement("li");
+		li.textContent = "Item " + count++;
+		list.appendChild(li);
+	}
+}
+addItems(1000);
+
+list.addEventListener("scroll", () => {
+	if(list.scrollTop + list.clientHeight >= list.scrollHeight){
+		addItems(2);
+	}
+})
